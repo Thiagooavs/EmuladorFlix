540a5685c9fb397c7736ad3936ccffa879575f96
Usuario <Usuario@CPD02-M11>
2024-04-30 11:43:10 -0300

    termino de link

--- EmuladorFlix/MainPage.xaml.cs
+++ EmuladorFlix/MainPage.xaml.cs
@@ -13,6 +13,61 @@
         {
             Navigation.PushAsync(new Categorias.Aventura());
         }
+
+        private void comedia_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Categorias.Comedia());
+        }
+
+        private void drama_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Categorias.Drama());
+        }
+
+        private void terror_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Categorias.Terror());
+        }
+
+        private void ficcao_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Categorias.Ficcao());
+        }
+
+        private void suspence_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Categorias.Suspence());
+        }
+
+        private void infantil_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Categorias.Infantil());
+        }
+
+        private void animacao_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Categorias.Animacao());
+        }
+
+        private void documentario_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Categorias.Documentario());
+        }
+
+        private void acao_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Categorias.acao());
+        }
+
+        private void nacional_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Categorias.Nacional());
+        }
+
+        private void romance_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Categorias.Romance());
+        }
     }
 
 }
